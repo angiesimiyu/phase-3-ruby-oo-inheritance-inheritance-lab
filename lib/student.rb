c2fdb6a7356c8910af1dc555b0d7459c90033033
angelinesimiyu <angie.lukelas@gmail.com>
2023-02-17 22:47:01 +0300

    Completed Assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn (knowledge)
+        @knowledge = knowledge
+    end
+
+    def knowledge
+        @knowledge
+    end
+
+    def self.knowledge
+        @knowledge
+    end
 
 end
